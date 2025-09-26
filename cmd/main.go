8ec4966c02ebcb3580c1d23aeed2417025eaa566
grian32 <grianguy32@gmail.com>
2025-09-26 20:26:13 +0300

    feat: ebiten hello world

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+type Test struct {
+}
+
+func (t *Test) Update() error {
+	return nil
+}
+
+func (t *Test) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "Hello, World!")
+}
+
+func (t *Test) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 640, 480
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Hello, World!")
+	if err := ebiten.RunGame(&Test{}); err != nil {
+		log.Fatalln(err)
+	}
 }
