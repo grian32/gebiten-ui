0bf0a589282033974c05cf63fcad891d5065a67c
grian32 <grianguy32@gmail.com>
2025-09-27 11:31:14 +0300

    feat: add updating for gbutton

--- button.go
+++ button.go
@@ -5,19 +5,21 @@ import (
 	"math"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 	"github.com/hajimehoshi/ebiten/v2/text/v2"
 )
 
 type GButtonClick func()
 
 type GButton struct {
-	text        string
-	textPos     Vec2
-	clickBounds Vec2
-	x, y        float64
-	font        *GFont
-	tex         *ebiten.Image
-	onClick     GButtonClick
+	text       string
+	textPos    Vec2
+	minX, minY int
+	maxX, maxY int
+	x, y       float64
+	font       *GFont
+	tex        *ebiten.Image
+	onClick    GButtonClick
 }
 
 // TODO: semi dupe code between these, but would end up measuring twice if i did bounds check then called NewButtonNoCheck
@@ -37,9 +39,17 @@ func NewButton(text string, x, y float64, tex *ebiten.Image, font *GFont, onClic
 	textX := (float64(texBounds.Dx()) - strWidth) / 2.0
 	textY := (float64(texBounds.Dy()) + strHeight + height) / 2.0
 
+	// float to int can be expensive, probably micro, but might aswell
+	intX := int(x)
+	intY := int(y)
+
 	return &GButton{
 		text:    text,
 		textPos: Vec2{X: textX, Y: textY},
+		minX:    intX,
+		minY:    intY,
+		maxX:    intX + texBounds.Dx(),
+		maxY:    intY + texBounds.Dy(),
 		x:       x,
 		y:       y,
 		tex:     tex,
@@ -59,9 +69,17 @@ func NewButtonNoCheck(text string, x, y float64, tex *ebiten.Image, font *GFont,
 	textX := (float64(texBounds.Dx()) - strWidth) / 2.0
 	textY := (float64(texBounds.Dy()) - strHeight - height) / 2.0
 
+	// float to int can be expensive, probably micro, but might aswell
+	intX := int(x)
+	intY := int(y)
+
 	return &GButton{
 		text:    text,
 		textPos: Vec2{X: textX, Y: textY},
+		minX:    intX,
+		minY:    intY,
+		maxX:    intX + texBounds.Dx(),
+		maxY:    intY + texBounds.Dy(),
 		x:       x,
 		y:       y,
 		tex:     tex,
@@ -71,7 +89,12 @@ func NewButtonNoCheck(text string, x, y float64, tex *ebiten.Image, font *GFont,
 }
 
 func (gb *GButton) Update() {
-
+	if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
+		cursorX, cursorY := ebiten.CursorPosition()
+		if cursorX >= gb.minX && cursorX <= gb.maxX && cursorY >= gb.minY && cursorY <= gb.maxY {
+			gb.onClick()
+		}
+	}
 }
 
 // Draw draws the button and attempts to center label within texture
